6ae8f11614d1b9a4fba67426cec1969fd2abd8e1
Tom <756424516@qq.com>
2021-10-20 11:43:40 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/tom/coolweather/util/Utility.java
@@ -0,0 +1,70 @@
+package com.tom.coolweather.util;
+
+import android.text.TextUtils;
+
+import com.tom.coolweather.db.City;
+import com.tom.coolweather.db.County;
+import com.tom.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+    //解析处理服务器返回的省数据
+    public static boolean handleProvinceResponse(String response){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.save();
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    //解析处理服务器返回的市数据
+    public static boolean handleCityResponse(String response,int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i<allProvinces.length();i++){
+                    JSONObject cityObject = allProvinces.getJSONObject(i);
+                    City city = new City();
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setCityName(cityObject.getString("name"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    //解析处理服务器返回的县数据
+    public static boolean handleCountyResponse(String response,int cityId){
+        if(!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i = 0;i<allProvinces.length();i++){
+                    JSONObject countyObject = allProvinces.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
