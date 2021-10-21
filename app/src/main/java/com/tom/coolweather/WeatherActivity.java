0ece1c3151ebb65ce9c91b1d1b90123db486f862
Tom <756424516@qq.com>
2021-10-21 10:40:24 +0800

    增加后台自动更新天气功能

--- app/src/main/java/com/tom/coolweather/WeatherActivity.java
+++ app/src/main/java/com/tom/coolweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.tom.coolweather.gson.Forecast;
 import com.tom.coolweather.gson.Weather;
+import com.tom.coolweather.service.AutoUpdateService;
 import com.tom.coolweather.util.HttpUtil;
 import com.tom.coolweather.util.Utility;
 
@@ -144,6 +146,8 @@ public class WeatherActivity extends AppCompatActivity {
                         editor.putString("weather",responseText);
                         editor.apply();
                         showWeatherInfo(weather);
+                        Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                        startService(intent);
                     }else{
                         Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
                     }
